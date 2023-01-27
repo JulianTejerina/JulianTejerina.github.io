a7bbdb080d282ac8926b04a2e1cffdd5b1ed1f52
Julian_Tejerina <42306033@fi.unju.edu.ar>
2023-01-27 00:55:00 -0300

    first commit

---
+++ js/Swiper.js
@@ -0,0 +1,33 @@
+
+
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+
+    },
+    pagination: {
+      el: '.swiper-pagination',
+
+    },
+    loop: true,
+  });
+  var swiper = new Swiper('.swiper-container-flip', {
+    effect: 'flip',
+    grabCursor: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+  });
